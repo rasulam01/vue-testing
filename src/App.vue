e5cb77efd6b4f607e01dee28a3b7647f3cd1c84e
rasul react web <imac@iMac-iMac.local>
2022-04-13 14:19:54 +0300

    first steps in vue learning

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ShitPants from './components/testingthings.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ShitPants
+  }
+}
+
+</script>
+
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="une belle vue" /> 
+  
+  <div>
+    <ShitPants v-bind:shit="5**5" />
+    <button>Plus</button>
+  </div>
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 90px;
+}
+</style>
---
+++ src/components/testingthings.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>Remember, you have got {{ shit }} seconds to run!</div>
+  <button :disabled="isDisabled">Spit</button>
+  <div @click="switchDisabled">
+    {{ disabledText }}
+  </div>
+  <div @click="increment" :class="[shit >= 30 ? classes.overThirty: shit >= 20 ? classes.overTwenty : shit >= 10 ? classes.overTen : classes.underTen]" >
+     Increase the value 
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'ShitPants',
+    data() {
+        return {
+            shit: 0,
+            isDisabled: false,
+            classes: {
+                overThirty: 'overthirty',
+                overTwenty: 'overtwenty',
+                overTen: 'overten',
+                underTen: 'underten'
+            }
+        }
+    },
+    methods: {
+        switchDisabled() {
+            this.isDisabled = !this.isDisabled
+        },
+        increment() {
+            this.shit += 10
+        }
+    },
+    computed: {
+        disabledText() {
+            return this.isDisabled ? 'Unblock the button' : 'Block the button'
+        }
+    }
+}
+
+</script>
+
+
+<style>
+
+.coco{
+    color: red;
+}
+
+.overthirty{
+    background: blue;
+    color: darkgreen;
+}
+.overtwenty{
+    background: darkgreen;
+    color: blue;
+}
+.overten{
+    background: black;
+    color: darkred;
+}
+.underten{
+    background: darkred;
+    color: black;
+}
+
+</style>
